60842acc4db53f49508f360df1eca1cee575438f
Jack Laurence <lawrencetecho@gmail.com>
2018-12-23 18:18:22 +0800

    Update index.js
    Changed metro-config blacklist directory to updated version.

--- metro-bundler-config-yarn-workspaces/index.js
+++ metro-bundler-config-yarn-workspaces/index.js
@@ -1,4 +1,4 @@
-const blacklist = require('metro/src/blacklist')
+const blacklist = require('metro-config/src/defaults/blacklist')
 const getWorkspaces = require('get-yarn-workspaces')
 const path = require('path')
 
